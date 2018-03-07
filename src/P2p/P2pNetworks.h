690d8366b6184467ae1f41403a2df01073aa6112
FOSB dev team <davidpan9999@gmail.com>
2018-03-07 19:19:13 +0700

    commit first time

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Copyright (c) 2017-2018 FOSB-project developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x41, 0x21, 0x18, 0x4f, 0x50, 0x5a, 0x5a, 0x12, 0x4f, 0x01, 0x18, 0x10, 0x54, 0x45, 0x22, 0xf3 } };
+}
